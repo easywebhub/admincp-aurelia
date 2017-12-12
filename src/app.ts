99907883e702dff391ae35c62de2dfddf3945f31
quocbao_home <quocbao.tn@gmail.com>
2017-12-12 22:25:20 +0700

    change display text

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,12 @@ export class App {
   router: Router;
 
   configureRouter(config: RouterConfiguration, router: Router) {
-    config.title = 'Aurelia';
+    config.title = 'EasyWeb';
     config.map([
       { route: ['', 'welcome'], name: 'welcome',      moduleId: PLATFORM.moduleName('./modules/common/welcome'),      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: PLATFORM.moduleName('./modules/users/users'),        nav: true, title: 'Github Users' },
-      { route: 'sample',         name: 'sample',        moduleId: PLATFORM.moduleName('./modules/samples/sample'),        nav: true, title: 'EasyWeb Sample' },
-      { route: 'child-router',  name: 'child-router', moduleId: PLATFORM.moduleName('./modules/common/child-router'), nav: true, title: 'Child Router' },
+      // { route: 'users',         name: 'users',        moduleId: PLATFORM.moduleName('./modules/users/users'),        nav: true, title: 'Github Users' },
+      { route: 'sample',         name: 'sample',        moduleId: PLATFORM.moduleName('./modules/samples/sample'),        nav: true, title: 'Case Study' },
+      // { route: 'child-router',  name: 'child-router', moduleId: PLATFORM.moduleName('./modules/common/child-router'), nav: true, title: 'Child Router' },
     ]);
 
     this.router = router;
