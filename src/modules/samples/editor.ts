9c76a5691ed8977d654a5b6174642adaaf4f3d70
quocbao_home <quocbao.tn@gmail.com>
2017-12-12 22:14:39 +0700

    add Editor

---
+++ src/modules/samples/editor.ts
@@ -0,0 +1,13 @@
+import { format } from "url";
+
+
+export class EwEditor {
+    
+   
+
+    constructor(){
+        // this.dateTimePicker.dateEntered = new Date(2017,12,3);
+    }  
+    activate() {
+    }
+}    
