c69cce16535d7de1966818eb2da01e2026b32338
quocbao_home <quocbao.tn@gmail.com>
2017-12-12 21:58:58 +0700

    add select2

--- src/modules/samples/sample.ts
+++ src/modules/samples/sample.ts
@@ -12,6 +12,20 @@ export class Sample {
     }    
     date1 : EWDateTimePicker = new EWDateTimePicker()
     date2 : EWDateTimePicker = new EWDateTimePicker()
+
+    selectedProduct : Product;     
+    selectedProducts : Product[];     
+    
+    products: Product[]  = [
+        new Product(1, 'Motherboard'),
+        new Product(2, 'CPU'),
+        new Product(3, 'Memory')
+    ];
+    /**
+    * link http://aurelia.io/docs/binding/selects#select-number
+    * 
+    */
+
     constructor(){
         // this.dateTimePicker.dateEntered = new Date(2017,12,3);
     }  
@@ -25,11 +39,22 @@ export class EWDateTimePicker {
     options : {
         format: 'DD-MM-YYYY'
     }
+
     constructor() {
         this.date = new Date();    
     }
 
+
     getDate() :Date {
         return new Date(this.date);    
+    }
+}
+
+export class Product {
+    id: number;
+    name: string;
+    constructor(id, name){
+        this.id = id;
+        this.name = name;    
     }
 }
