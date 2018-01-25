c3408ede865ec444407c3358edb981ec1a6d2626
quocbao_home <quocbao.tn@gmail.com>
2018-01-25 18:07:43 +0700

    update style

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ export class App {
         script.src = file;
         script.type = 'text/javascript';
         script.defer = true;
-        document.getElementsByTagName('head').item(0).appendChild(script);
+        document.getElementsByTagName('body').item(0).appendChild(script);
 
         script.onload = function () {
           resolve()
