9e73b6bed4a053d79391cd04c4e3a054ce1140e3
quocbao_home <quocbao.tn@gmail.com>
2018-01-29 23:22:43 +0700

    aurelia dialog

--- src/modules/samples/sample.ts
+++ src/modules/samples/sample.ts
@@ -14,7 +14,7 @@ export class Sample {
             { route: ['','datetime'], name: 'datetime', moduleId: PLATFORM.moduleName('./datetime'), nav: true, title: 'DateTimePicker' },
             { route: 'ew-select', name: 'ew-select', moduleId: PLATFORM.moduleName('./select'), nav: true, title: 'Select' },
             { route: 'ew-editor', name: 'ew-editor', moduleId: PLATFORM.moduleName('./editor'), nav: true, title: 'Editor' },
-            // { route: 'ew-dialog', name: 'ew-dialog', moduleId: PLATFORM.moduleName('./dialog'), nav: true, title: 'Dialog(Modal)' },
+            { route: 'ew-dialog', name: 'ew-dialog', moduleId: PLATFORM.moduleName('./dialog'), nav: true, title: 'Dialog(Modal)' },
             { route: 'listUser', name: 'listUser', moduleId: PLATFORM.moduleName('./users/listUser'), nav: true, title: 'EasyWeb Users' },
             { route: 'gmaps', name: 'gmaps', moduleId: PLATFORM.moduleName('./googlemaps'), nav: true, title: 'Google Maps' },
             { route: 'tree', name: 'ew-tree', moduleId: PLATFORM.moduleName('./tree'), nav: true, title: 'Tree View' },
